c41a16e420aa52ec4ac6ad02a22c08d67753f045
Doppeltje <Doppeltje@users.noreply.github.com>
2018-11-21 19:23:16 +0100

    first commit

---
+++ Assets/Scripts/GameScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScript : MonoBehaviour
+{
+    public Questions[] question;
+
+    public Text questionText;
+    public Text answer1Text;
+    public Text answer2Text;
+    public Text answer3Text;
+
+    private int questionCount = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        questionText.text = question[questionCount].question;
+        answer1Text.text = question[questionCount].answers[0];
+        answer2Text.text = question[questionCount].answers[1];
+        answer3Text.text = question[questionCount].answers[2];
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            questionCount++;
+            NextQuestion();
+        }
+    }
+
+    private void NextQuestion()
+    {
+        questionText.text = question[questionCount].question;
+        answer1Text.text = question[questionCount].answers[0];
+        answer2Text.text = question[questionCount].answers[1];
+        answer3Text.text = question[questionCount].answers[2];
+    }
+}
---
+++ Assets/Scripts/Questions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Question", menuName = "Questions")]
+public class Questions : ScriptableObject
+{
+    public string question;
+    public string[] answers;
+
+    public Sprite artwork;
+}
